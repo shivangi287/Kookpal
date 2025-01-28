81bab78d4d644ef62b724f3d0347b3dd98eca32b
shivangi <Shivangi.Sharma@student.ufv.ca>
2025-01-28 11:34:33 -0800

    config

--- kookpal-frontend-new/src/config.js
+++ kookpal-frontend-new/src/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://kookpal.onrender.com/';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://kookpal.onrender.com';
